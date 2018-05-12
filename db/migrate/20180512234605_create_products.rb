824bc20f735e5d42c86ad896fadf3f382485d714
Matt <matthewprimpas@gmail.com>
2018-05-12 19:49:13 -0400

    Fix Product schema and migrate to database

--- db/migrate/20180512234605_create_products.rb
+++ db/migrate/20180512234605_create_products.rb
@@ -2,8 +2,7 @@ class CreateProducts < ActiveRecord::Migration[5.1]
   def change
     create_table :products do |t|
       t.string :name
-      t.string :description
-      t.string :text
+      t.text :description
       t.string :image_url
       t.integer :price
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180512233015) do
+ActiveRecord::Schema.define(version: 20180512234605) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.string "description"
-    t.string "text"
+    t.text "description"
     t.string "image_url"
     t.integer "price"
     t.datetime "created_at", null: false
