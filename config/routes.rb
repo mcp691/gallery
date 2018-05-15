a28b2af03ccc1b4f0a15d829d612212db74e0990
Matt <matthewprimpas@gmail.com>
2018-05-15 10:22:39 -0400

    Add and style Contact form that posts to Thank You page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
   get 'static_pages/contact'
 
+  post 'static_pages/thank_you'
+
   root 'static_pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
