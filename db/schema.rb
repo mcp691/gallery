e4a62d7ee00ba411fa37ba11f8a40bf1e43ba5bc
Matt <matthewprimpas@gmail.com>
2018-05-18 21:06:39 -0400

    Add Category column to Product table in database and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180516132701) do
+ActiveRecord::Schema.define(version: 20180519005718) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20180516132701) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
   end
 
   create_table "users", force: :cascade do |t|
