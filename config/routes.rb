0d6d833b18e7184f69f3501167d1cd1a48b5a797
Matt <matthewprimpas@gmail.com>
2018-06-16 21:25:00 -0400

    Rename routes to simplify URL and hide static_pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,12 @@ Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' }
   resources :users
   resources :products
-  
-  get 'static_pages/home'
 
-  get 'static_pages/about'
+  get '/about', to: 'static_pages#about'
 
-  get 'static_pages/contact'
+  get '/contact', to: 'static_pages#contact'
 
-  post 'static_pages/thank_you'
+  post '/thank_you', to: 'static_pages#thank_you'
 
   root 'static_pages#home'
 
